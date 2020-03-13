db9579ba8ae273fd205bcd6c1bf5550bfedf4137
Richard Ramsey <rramsey@paveaway.com>
2020-03-13 09:44:42 -0500

    First code commit

---
+++ SqliteIntro/DataTableExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace SqliteIntro
+{
+    public static class DataTableExtension
+    {
+        public static string ToCSV(this DataTable dt)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().Select(column => column.ColumnName);
+            sb.AppendLine(string.Join(",", columnNames));
+
+            foreach (DataRow row in dt.Rows)
+            {
+                IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                sb.AppendLine(string.Join(",", fields));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ SqliteIntro/Program.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SqliteIntro
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var app = new CommandLineApplication
+            {
+                Name = "SqliteIntro",
+                FullName = "Sqlite Execute Query",
+                Description = "Executes queries from the command line",
+            };
+            app.HelpOption("-?|-h|--help");
+
+            // SqliteIntro -f "c:\DB\database.sqlite" -q "SELECT * FROM tasks" -o test.csv
+
+              var output = app.Option("-o|--output <FILEPATH>", "Path to save CSV to.", CommandOptionType.SingleValue);
+            var query = app.Option("-q|--query <QUERY>", "Sql query to execute", CommandOptionType.SingleValue);
+            var file = app.Option("-f|--file <FILEPATH>", "Path and file name of Sqlite database", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                Console.WriteLine($"Executing query: {query.Value()} from DB: {file.Value()} ");
+
+                if (!File.Exists(file.Value()))
+                {
+                    return 2; // File not found
+                }
+                string connectionString = $"Data Source = {file.Value()}";
+
+                var dt = SqliteDB(connectionString, query.Value());
+                if (dt.Rows.Count > 0)
+                {
+                    File.WriteAllText(output.Value(), dt.ToCSV());
+                    Console.WriteLine($"CSV file written: {Path.GetFullPath(output.Value())}");
+                }
+                else
+                {
+                    Console.WriteLine("Query produced no result.");
+                }
+                var exitCode = 0; // program executed successfull
+                return exitCode;
+            });
+
+
+            try
+            {
+                return app.Execute(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ToString());
+            }
+
+            return 1; // program failed 
+        }
+
+        public static DataTable SqliteDB(string connString, string query)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqliteConnection conn = new SqliteConnection(connString))
+            using (SqliteCommand cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandText = query;
+                SqliteDataReader reader = cmd.ExecuteReader();
+                dataTable.Load(reader);
+            }
+            return dataTable;
+        }
+    }
+
+}
+
+
+//string connectionString = @"Data Source= \\cmsdev\c$\ProgramData\Advanced Task Scheduler Network\allusers.sqlite";
+//string query = "SELECT * FROM tasks";
+
+//var dt = SqliteDB(connectionString, query);
+
+
+//Console.WriteLine("Hello World!");
+//        }
